c90d24e158e67522084090eb105ee8c7c6558297
Hideaki Tai <hideaki.tai@gmail.com>
2019-01-04 19:15:16 +0900

    fix warning for teensy

--- Packetizer.h
+++ Packetizer.h
@@ -10,11 +10,12 @@ extern "C"
     int _write() { return -1; }
 }
 // DIRTY for TEENSYDUINO compile...
+// copied from https://github.com/gcc-mirror/
 namespace std
 {
-    void __throw_bad_alloc() { Serial.println("Unable to allocate memory"); }
-    void __throw_length_error(char const* e) { Serial.print("Length Error :"); Serial.println(e); }
-    void __throw_bad_function_call() {}
+    void __throw_bad_alloc() { _GLIBCXX_THROW_OR_ABORT(bad_alloc()); }
+    void __throw_length_error(const char* __s __attribute__((unused))) { _GLIBCXX_THROW_OR_ABORT(length_error(_(__s))); }
+    void __throw_bad_function_call() { _GLIBCXX_THROW_OR_ABORT(bad_function_call()); }
     // void _Rb_tree_decrement(std::_Rb_tree_node_base* a) {}
     // void _Rb_tree_insert_and_rebalance(bool, std::_Rb_tree_node_base*, std::_Rb_tree_node_base*, std::_Rb_tree_node_base&) {}
 }
